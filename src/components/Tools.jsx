c8eba5282f182d986ecdec07086c29de84b5917b
Abdul-Qudus Olatunji <fikewabdul@gmail.com>
2023-09-12 01:34:57 +0100

    tools

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -5,7 +5,7 @@ const Tools = () => {
     return (
         <div>
             <h1 className='text-primary font-semibold text-3xl mt-14'>The Tools I work with</h1>
-            <div className='grid grid-cols-3 gap-5 sm:grid-cols-3 md:grid-cols-6 my-8 md:mx-16'>
+            <div className='grid grid-cols-3 gap-5 sm:grid-cols-3 md:grid-cols-6 my-8 md:mx-40'>
                 {
                     tools.map(tool=> <div key={tool.id} className='tool-card flex flex-col w-24 h-28 justify-center items-center p-6 text-center text-black bg-tertiary'>
                         <img src={tool.image} alt="" className='mb-3'/>
