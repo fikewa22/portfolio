4d2075434713f95f4822c63a9d1456e2735bd2fa
Abdul-Qudus Olatunji <fikewabdul@gmail.com>
2023-10-14 01:00:40 +0100

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,11 +13,18 @@ const Projects = () => {
       repo: "https://github.com/fikewa22/little-lemon.git"
     },
     {
-      title: "ROBOFRIENDS",
-      description: "A fun and simple website to search for robot users.",
+      title: "FELIDAE",
+      description: "A brief exploration of the fascinating world of feline creatures, from domestic cats to their wild counterparts, in a simple and informative format.",
       photo: img2,
-      live: "https://robofriends-fikewa.vercel.app/",
+      live: "https://felidae.vercel.app/",
       repo: "https://github.com/fikewa22/robofriends.git"
+    },
+    {
+      title: "LEAPCURE",
+      description: "A patient-centered platform bridging the gap between clinical trials and those who need them most, fostering equitable research through patient advocacy.",
+      photo: img3,
+      live: "https://leapcure.vercel.app/",
+      repo: "https://github.com/fikewa22/leapcure.git"
     }
   ];
   return (
@@ -26,7 +33,7 @@ const Projects = () => {
       <p className='text-black my-2 md:w-2/3 leading-[2]'>
 I have worked on many projects over the course of being a web developer. Here are a few of my live, real-world projects. </p>
 {/* Featured Projects */}
-<div className="grid grid-cols-1 md:grid-cols-2 md:gap-[100px] sm:mx-9 my-6 items-center justify-center">
+<div className="grid grid-cols-1 md:grid-cols-3 md:gap-[100px] sm:mx-9 my-6 items-center justify-center">
   {
     projects.map((project,index)=> <div
     key={index}
@@ -40,10 +47,10 @@ I have worked on many projects over the course of being a web developer. Here ar
     >
       <img src={project.photo} alt={project.title} />
     </a>
-    <h3 className="text-primary font-semibold text-xl pt-8">
+    <h3 className="text-primary font-semibold text-xl">
       {project.title}
     </h3>
-    <p className=" text-black mt-1 leading-[2]">{project.description}</p>
+    <p className="relative text-black mt-1 leading-[2]">{project.description}</p>
     <div className="flex flex-col md:block justify-start items-center mt-5 md:mt-[350px] md:absolute">
       <a href={project.live}><button className="btn transition-all duration-500 bg-primary py-2 px-6 rounded text-white hover:bg-white hover:text-primary">
 View Live</button></a>
