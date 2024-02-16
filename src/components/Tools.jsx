5e24610ad65490e31dc36a85e06b20c19cdc7363
Abdul-Qudus Olatunji <fikewabdul@gmail.com>
2024-02-16 22:48:39 +0100

    optimized

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -3,14 +3,13 @@ import tools from '../../dist/tools'
 
 const Tools = () => {
     return (
-        <div className='gap-2 border-2 border-red-600'>
+        <div>
             <h1 className='text-primary font-semibold text-3xl mt-14'>The Tools I work with</h1>
             <div className='grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-6 my-8 md:mx-40 sm:items-center sm:justify-center'>
                 {
                     tools.map(tool=> <div key={tool.id} className='tool-card transform hover: transition duration-500 hover:scale-125 flex flex-col w-24 h-28 justify-center items-center p-6 text-center text-black bg-tertiary'>
                         <img src={tool.image} alt="" className='mb-3'/>
                         <h6 className='text-white text-sm mb-2'>{tool.name}</h6>
-                        {/* <p className='text-base'>{tool.description}</p> */}
                     </div>)
                 }
             </div>
