cea449782b44b38f8f83acac513982506a7f4ab1
Abdul-Qudus Olatunji <fikewabdul@gmail.com>
2025-08-04 07:18:11 -0500

    updated style

--- dist/tools.js
+++ dist/tools.js
@@ -6,12 +6,12 @@ import IMG5 from "../src/assets/icons/github.svg"
 import IMG6 from "../src/assets/icons/git.svg"
 
 const tools = [
-    {id:1, name:'Javascript', image:IMG1},
-    {id:2, name:'CSS 3', image:IMG2},
-    {id:3, name:'HTML5', image:IMG3},
-    {id:4, name:'ReactJS', image:IMG4},
-    {id:5, name:'Github', image:IMG5},
-    {id:6, name:'Git', image:IMG6},
+    {id:1, name:'Javascript', description: 'I craft engaging web pages using the power of HTML5, CSS, and JavaScript to bring your ideas to life.', image:IMG1},
+    {id:2, name:'CSS 3', description:'I create professional layouts using industry-standard tools like Tailwind CSS, ensuring your website looks stunning across all devices.', image:IMG2},
+    {id:3, name:'HTML 5', description:'I design intuitive user interfaces that prioritize user experience, leveraging my expertise in React and Figma.', image:IMG3},
+    {id:4, name:'ReactJS', description:'I manage projects effectively using Git repositories and version control, ensuring seamless collaboration and organized development.', image:IMG4},
+    {id:5, name:'Github', description:'I build dynamic applications with React, utilizing routing, hooks, bundlers, and data fetching to deliver exceptional user interactions.', image:IMG5},
+    {id:6, name:'Git', description:"I enhance your website's visual appeal by skillfully editing images using Figma, making sure your content stands out.", image:IMG6},
 ]
 
 export default tools;
